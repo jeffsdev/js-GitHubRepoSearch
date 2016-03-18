99024bcaf016bf91ef4e1b4a4b53ce01d89d1516
Jeff Seymour <seym1585@gmail.com>
2016-03-18 11:38:06 -0700

    add functionality to retrieve and display initial user data and repo data

--- js/github.js
+++ js/github.js
@@ -21,20 +21,20 @@ exports.getUserInfo = function(){
 };
 
 
-// Get Repository Names and Details
-// exports.getRepos = function(){
-//   $.get('https://api.github.com/users/jeffsdev/repos?access_token=' + apiKey + '&per_page=9999;sort="updated"').then(function(response){
-//     console.log(response);
-//
-//     response.forEach(function(repository, i) {
-//       $('.user-repo-list ul').append("<li class='gh-reponame' id='repo" + i + "'>" + "<a href='"+ repository.html_url +"' target='_blank'>" + repository.name + "</a></li>");
-//       $('#repo' + i).append("<div class='gh-repodesc' id='repo-desc" + i + "'>" + repository.description + "</div>");
-//     });
-//
-//
-//
-//
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
+Get Repository Names and Details
+exports.getRepos = function(){
+  $.get('https://api.github.com/users/jeffsdev/repos?access_token=' + apiKey + '&per_page=9999;sort="updated"').then(function(response){
+    console.log(response);
+
+    response.forEach(function(repository, i) {
+      $('.user-repo-list ul').append("<li class='gh-reponame' id='repo" + i + "'>" + "<a href='"+ repository.html_url +"' target='_blank'>" + repository.name + "</a></li>");
+      $('#repo' + i).append("<div class='gh-repodesc' id='repo-desc" + i + "'>" + repository.description + "</div>");
+    });
+
+
+
+
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};

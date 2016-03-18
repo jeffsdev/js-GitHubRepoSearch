8b091d1715f73cac10cc5e7f78b06b21e3565572
Jeff Seymour <seym1585@gmail.com>
2016-03-18 11:37:12 -0700

    add interface functionality

--- js/github.js
+++ js/github.js
@@ -1,7 +1,40 @@
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+var apiKey = require('./../.env').apiKey;
+
+// Get GitHub User Information
+exports.getUserInfo = function(){
+  $.get('https://api.github.com/users/' + userInput + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+
+    userRepos = response.repos_url;
+
+    userName = response.login;
+    userRealName = response.name;
+    userLocation = response.location;
+
+    $('.gh-username').text(userName);
+    $('.gh-realname').text(userRealName);
+    $('.gh-location').text(userLocation);
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
+
+
+// Get Repository Names and Details
+// exports.getRepos = function(){
+//   $.get('https://api.github.com/users/jeffsdev/repos?access_token=' + apiKey + '&per_page=9999;sort="updated"').then(function(response){
+//     console.log(response);
+//
+//     response.forEach(function(repository, i) {
+//       $('.user-repo-list ul').append("<li class='gh-reponame' id='repo" + i + "'>" + "<a href='"+ repository.html_url +"' target='_blank'>" + repository.name + "</a></li>");
+//       $('#repo' + i).append("<div class='gh-repodesc' id='repo-desc" + i + "'>" + repository.description + "</div>");
+//     });
+//
+//
+//
+//
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
--- js/user-interface.js
+++ js/user-interface.js
@@ -1,5 +1,18 @@
-var apiKey = require('./../.env').apiKey;
+var getUserInfo = require('./../js/github.js').getUserInfo;
 var getRepos = require('./../js/github.js').getRepos;
 
 
-getRepos();
+$(document).ready(function() {
+
+  $('.gh-form').submit(function(event) {
+    event.preventDefault();
+    userInput = $('.user-input').val();
+    getUserInfo();
+    getRepos();
+
+
+  });
+
+
+
+});
